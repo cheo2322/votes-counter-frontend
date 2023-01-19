ffceadc69b7785f0de957451de1fd14a14721a90
Cheo <barcecheo2319@gmail.com>
2023-01-18 22:58:43 -0500

    testing local API Rest

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function App() {
+  const [votes, setVotes] = useState();
+
+  useEffect(() => {
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+
+    axios
+      .put(
+        `http://abcd/counter_api/v1/candidate/63c89e48dd07ce2f83d58ebe/votes/add?votes=1`,
+        { headers }
+      )
+      .then((res) => setVotes(res.data))
+      .catch((error) => {
+        if (error.response) {
+          console.error(error.response.data);
+        } else {
+          console.error(error);
+        }
+      });
+
+    return () => {
+      setVotes();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{votes}</Text>
       <StatusBar style="auto" />
     </View>
   );
