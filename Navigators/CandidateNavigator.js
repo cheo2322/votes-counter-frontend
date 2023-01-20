c4bcddfb6726261e9f8fd951dd5362b799e1d9e6
Cheo <barcecheo2319@gmail.com>
2023-01-19 21:54:24 -0500

    adding new candidate (only front)

--- Navigators/CandidateNavigator.js
+++ Navigators/CandidateNavigator.js
@@ -1,18 +1,73 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+
+import FormContainer from '../Shared/Form/FormContainer';
+import Input from '../Shared/Form/Input';
 
 const CandidateNavigator = () => {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [list, setList] = useState('');
+  const [city, setCity] = useState('');
+  const [province, setProvince] = useState('');
+  const [position, setPosition] = useState('');
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Hello, world!</Text>
-    </View>
+    <FormContainer title={''}>
+      <Input
+        placeholder={'Nombre'}
+        name={'name'}
+        id={'name'}
+        value={name}
+        onChangeText={(text) => setName(text)}
+      />
+      <Input
+        placeholder={'Apellido'}
+        name={'lastName'}
+        id={'lastName'}
+        value={lastName}
+        onChangeText={(text) => setLastName(text)}
+      />
+      <Input
+        placeholder={'Lista'}
+        name={'list'}
+        id={'list'}
+        value={list}
+        onChangeText={(text) => setList(text)}
+      />
+      <Input
+        placeholder={'Ciudad'}
+        name={'city'}
+        id={'city'}
+        value={city}
+        onChangeText={(text) => setCity(text)}
+      />
+      <Input
+        placeholder={'Provincia'}
+        name={'province'}
+        id={'province'}
+        value={province}
+        onChangeText={(text) => setProvince(text)}
+      />
+      <Input
+        placeholder={'Delegación'}
+        name={'position'}
+        id={'position'}
+        value={position}
+        onChangeText={(text) => setPosition(text)}
+      />
+      <View style={[{ marginBottom: 40 }, styles.buttonGroup]}>
+        <Button title="Enviar"></Button>
+      </View>
+    </FormContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  buttonGroup: {
+    width: '80%',
+    alignItems: 'center',
+  },
+});
 
 export default CandidateNavigator;
