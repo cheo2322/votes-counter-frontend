4c13f98439f942806cfc240d6942eb94e0d18b60
Cheo <barcecheo2319@gmail.com>
2023-01-20 20:53:09 -0500

    Adding checkbox to position

--- Navigators/CandidateNavigator.js
+++ Navigators/CandidateNavigator.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { View, StyleSheet, Button } from 'react-native';
+import { Select, Icon, CheckIcon, NativeBaseProvider } from 'native-base';
 
 import FormContainer from '../Shared/Form/FormContainer';
 import Input from '../Shared/Form/Input';
 
 import { BACKEND_URL } from '@env';
 
+const positions = ['ALCALDE', 'PREFECTO', 'CONCEJAL'];
+
 const CandidateNavigator = () => {
   const [name, setName] = useState('');
   const [lastName, setLastName] = useState('');
   const [list, setList] = useState('');
-  const [city, setCity] = useState('');
-  const [province, setProvince] = useState('');
   const [position, setPosition] = useState('');
 
   const postCandidate = () => {
@@ -30,8 +31,6 @@ const CandidateNavigator = () => {
         name: name,
         lastName: lastName,
         list: list,
-        city: city,
-        province: province,
         position: position,
       };
 
@@ -43,8 +42,6 @@ const CandidateNavigator = () => {
           setName('');
           setLastName('');
           setList('');
-          setCity('');
-          setProvince('');
           setPosition('');
         })
         .catch((error) => {
@@ -55,54 +52,54 @@ const CandidateNavigator = () => {
   };
 
   return (
-    <FormContainer title={''}>
-      <Input
-        placeholder={'Nombre'}
-        name={'name'}
-        id={'name'}
-        value={name}
-        onChangeText={(text) => setName(text)}
-      />
-      <Input
-        placeholder={'Apellido'}
-        name={'lastName'}
-        id={'lastName'}
-        value={lastName}
-        onChangeText={(text) => setLastName(text)}
-      />
-      <Input
-        placeholder={'Lista'}
-        name={'list'}
-        id={'list'}
-        value={list}
-        keyboardType={'numeric'}
-        onChangeText={(text) => setList(text)}
-      />
-      <Input
-        placeholder={'Ciudad'}
-        name={'city'}
-        id={'city'}
-        value={city}
-        onChangeText={(text) => setCity(text)}
-      />
-      <Input
-        placeholder={'Provincia'}
-        name={'province'}
-        id={'province'}
-        value={province}
-        onChangeText={(text) => setProvince(text)}
-      />
-      <Input
-        placeholder={'Delegación'}
-        name={'position'}
-        id={'position'}
-        value={position}
-        onChangeText={(text) => setPosition(text)}
-      />
-      <View style={[{ marginBottom: 40 }, styles.buttonGroup]}>
-        <Button title="Enviar" onPress={postCandidate}></Button>
-      </View>
-    </FormContainer>
+    <NativeBaseProvider>
+      <FormContainer>
+        <Input
+          placeholder={'Nombre'}
+          name={'name'}
+          id={'name'}
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+        <Input
+          placeholder={'Apellido'}
+          name={'lastName'}
+          id={'lastName'}
+          value={lastName}
+          onChangeText={(text) => setLastName(text)}
+        />
+        <Input
+          placeholder={'Lista'}
+          name={'list'}
+          id={'list'}
+          value={list}
+          keyboardType={'numeric'}
+          onChangeText={(text) => setList(text)}
+        />
+        <Select
+          minWidth="80%"
+          borderColor={'orange.300'}
+          height={60}
+          borderRadius={20}
+          color={'white'}
+          borderWidth={2}
+          placeholderStyle={{ color: '#007aff' }}
+          placeholderIconColor="#007aff"
+          iosIcon={<Icon name="arrow-down" color={'#007aff'} />}
+          accessibilityLabel="Selecciona una opción"
+          placeholder="Selecciona una opción"
+          selectedValue={position}
+          onValueChange={(e) => setPosition(e)}
+        >
+          {positions.map((p) => {
+            return <Select.Item key={p} label={p} value={p} />;
+          })}
+        </Select>
+        <View style={styles.buttonGroup}>
+          <Button title="Enviar" onPress={postCandidate}></Button>
+        </View>
+      </FormContainer>
+    </NativeBaseProvider>
   );
 };
 
@@ -110,6 +107,17 @@ const styles = StyleSheet.create({
   buttonGroup: {
     width: '80%',
     alignItems: 'center',
+    margin: 40,
+  },
+  input: {
+    width: '80%',
+    height: 60,
+    backgroundColor: 'white',
+    margin: 10,
+    borderRadius: 20,
+    padding: 10,
+    borderWidth: 2,
+    borderColor: 'orange',
   },
 });
 
