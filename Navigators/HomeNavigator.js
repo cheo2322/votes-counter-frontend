11d21610bc43b524684ff4f7f8781164595d7dc7
Cheo <barcecheo2319@gmail.com>
2023-01-22 00:09:42 -0500

    implementing modal for candidates

--- Navigators/HomeNavigator.js
+++ Navigators/HomeNavigator.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import {
   View,
@@ -7,16 +7,19 @@ import {
   SafeAreaView,
   StyleSheet,
   FlatList,
+  Pressable,
+  Modal,
 } from 'react-native';
 import { BACKEND_URL } from '@env';
 
 const HomeNavigator = () => {
-  const [refreshing, setRefreshing] = React.useState(false);
+  const [refreshing, setRefreshing] = useState(false);
   const [candidates, setCandidates] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [focusCandidate, setFocusCandidate] = useState({});
 
-  const onRefresh = React.useCallback(() => {
+  const onRefresh = useCallback(() => {
     setRefreshing(true);
-
     getCandidates();
 
     setTimeout(() => {
@@ -46,20 +49,83 @@ const HomeNavigator = () => {
         padding: 10,
       }}
     >
-      <Text>
-        Candidato: {item.name} {item.lastName} {'\n'}
-        Delegaci&oacute;n: {item.position} {'\n'}
-        Lista: {item.list} {'\n'}
-        Votos Totales: {item.votes}
-      </Text>
+      <Pressable
+        key={index}
+        onPress={() => {
+          setModalVisible(true);
+          setFocusCandidate(item);
+        }}
+      >
+        <Text>
+          Candidato: {item.name} {item.lastName} {'\n'}
+          Delegaci&oacute;n: {item.position} {'\n'}
+          Lista: {item.list} {'\n'}
+          Votos Totales: {item.totalVotes}
+        </Text>
+      </Pressable>
     </View>
   );
 
   return (
     <SafeAreaView style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            {focusCandidate ? (
+              <View key={focusCandidate.id}>
+                <Text style={styles.modalText}>
+                  {focusCandidate.name} {focusCandidate.lastName}
+                </Text>
+                <Text>{focusCandidate.position}</Text>
+                <Text>Lista {focusCandidate.list}</Text>
+                <Text>{focusCandidate.totalVotes} votos totales</Text>
+
+                {focusCandidate.votes ? (
+                  <View style={{ paddingTop: 10, paddingBottom: 10 }}>
+                    <Text>Votos por parroquias:</Text>
+                    {focusCandidate.votes.map((item, index) => {
+                      return (
+                        <Text key={index}>
+                          {item.parish} : {item.votesAmount}
+                        </Text>
+                      );
+                    })}
+                  </View>
+                ) : (
+                  <Text style={{ paddingTop: 10, paddingBottom: 10 }}>
+                    No hay información de los votos por parroquias
+                  </Text>
+                )}
+              </View>
+            ) : (
+              <Text>Lo sentimos. No pudimos consultar la información</Text>
+            )}
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+                setFocusCandidate({});
+              }}
+            >
+              <Text style={styles.textStyle}>Cerrar</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+
       <FlatList
         data={candidates}
-        renderItem={({ item, index }) => <Item item={item} index={index} />}
+        renderItem={({ item, index }) => (
+          <Item item={item} index={index} key={item.id} />
+        )}
         keyExtractor={(item) => item.id}
         refreshControl={
           <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
@@ -73,6 +139,47 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     borderRadius: 10,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 10,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
 
